66669295a1591db543c4a92205d3d131f850d348
dapeshcode <ampeccia@gmail.com>
2021-03-12 10:29:32 -0600

    small changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,26 @@ PlaceCollection.create(collection_id: 1, place_id: 1, name: "one day")
 PlaceCollection.create(collection_id: 1, place_id: 2, name: "one day")
 PlaceCollection.create(collection_id: 1, place_id: 3, name: "one day")
 
+
+##### these are the cities we are searching by
+
 cities = ["Chicago", "New Orleans", "Dallas", "Las Vegas", "Charleston", "Nashville"]
 
+##### this is the Yelp Fusion gem going to work
 client = Yelp::Fusion::Client.new(ENV["API_KEY"])
 yelp_params = {
     term: 'bars',
-    limit: 30
+    limit: 100
   }
+
+  ###### ^above the terms for the request: 100 bars for each city
+
+  ##### loop through the array of cities and make a request for each city in the array
  cities.each do |city|
     response = client.search(city, yelp_params)
-    # name = response.businesses[0].name
+   
 
+    ##### traverse through the data to grab attributes to create an instance of a new place for every business returned from the request
     response.businesses.each do |business|
         name = business.name
         state = business.location.state
@@ -40,18 +49,7 @@ yelp_params = {
     
  end 
 
-# def self.search 
-#     url = "https://api.yelp.com/v3/businesses/search"
-    
-#     yelp_params = {
-#     term: 'bars',
-#     limit: 1
-#   }
 
-#     response = HTTP.auth("Bearer #{ENV["API_KEY"]}").get(url, yelp_params)
-#     response.parse 
-#     byebug
-# end
 
 
 
